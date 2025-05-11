b14a582e86b223515d27fd06fb6aa65bfed920de
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-05-11 22:27:25 +0530

    area of circle codeforces

---
+++ E-Area_of_circle/main.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    double pi=3.141592653;
+    double r;
+    cin>>r;
+    double area=pi*(r*r);
+    cout<<fixed<<setprecision(9)<<area;
+}
