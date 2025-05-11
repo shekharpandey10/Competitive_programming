49dc4c7b9c72755374b7ea8dc56fccd5112dbbd7
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-05-11 21:33:52 +0530

    first two codeforces.com problem

---
+++ B-basic_data_type/main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i;
+    long long ll;
+    char ch;
+    float fl;
+    double dl;
+    cin>>i>>ll>>ch>>fl>>dl;
+    cout<<i<<endl;
+    cout<<ll<<endl;
+    cout<<ch<<endl;
+    cout<<fl<<endl;
+    cout<<dl<<endl;
+}
